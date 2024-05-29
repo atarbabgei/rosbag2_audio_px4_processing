1a94af8161dcce1c46f41e5c6737908755c944e2
atarbabgei <atarbabgei@gmail.com>
2024-05-29 16:38:07 +0100

    implement argparse

--- export_to_wav.py
+++ export_to_wav.py
@@ -1,21 +1,28 @@
 '''
 Description: This script reads audio data from a rosbag file and saves it to a .wav file.
-Install by running: 'pip3 install rosbags pydub'
 
-Usage: python3 export_to_wav.py
+Dependencies:
+- FFmpeg: Required for exporting audio data to a .wav file.
+- Python packages: rosbags, pydub
+
+'sudo apt-get install ffmpeg'
+'pip3 install rosbags pydub'
+
+Usage: python3 export_to_wav.py --dir /path/to/your/rosbag/folder
+Example: python3 export_to_wav.py --dir ~/Documents/rosbags/rosbag2_2024_05_29-15_46_13
 
 Assumption: The audio data is published under the topic /audio/audio using the message type audio_common_msgs/msg/AudioData.
 '''
 
+import argparse
+import os
+import sys
 from rosbags.rosbag2 import Reader
 from rosbags.typesys import get_typestore, Stores, get_types_from_idl
 from pydub import AudioSegment
-import numpy as np
 
 # Constants
-BAG_PATH = '/home/atar/Documents/rosbag-audio-beemav/rosbag2_2024_05_29-15_46_13'  # Path to the rosbag file
 AUDIO_TOPIC = '/audio/audio'  # Audio topic
-OUTPUT_FILE = 'output_audio.wav'  # Output audio file
 
 # AudioData message definition (example definition, adjust according to your actual message definition)
 AUDIO_DATA_MSG = """
@@ -62,12 +69,24 @@ def save_audio_to_file(audio_data, output_file):
 
 def main():
     """Main function to read the rosbag, collect audio data, and save it to a file."""
-    with Reader(BAG_PATH) as reader:
+    parser = argparse.ArgumentParser(description='Read audio data from a rosbag and save it to a .wav file.')
+    parser.add_argument('--dir', type=str, required=True, help='Path to the rosbag file')
+
+    args = parser.parse_args()
+    bag_path = args.dir
+
+    if not os.path.exists(bag_path):
+        print(f"Error: The file '{bag_path}' does not exist.")
+        sys.exit(1)
+
+    output_file = os.path.splitext(os.path.basename(bag_path))[0] + '.wav'
+
+    with Reader(bag_path) as reader:
         print("Reading audio data from the bag file...")
         audio_data = collect_audio_data(reader, AUDIO_TOPIC)
         
         print("Saving audio data to file...")
-        save_audio_to_file(audio_data, OUTPUT_FILE)
+        save_audio_to_file(audio_data, output_file)
 
 if __name__ == "__main__":
     main()
